bc59ad6207964b814419997eb3e2d230a142e3e0
Josh Wickster <joshwickster@gmail.com>
2020-12-09 09:45:19 -0700

    add

---
+++ Develop/seeds/category-seeds.js
@@ -0,0 +1,26 @@
+/* jshint esversion: 6 */
+
+const {
+    Category
+} = require('../models');
+
+const categoryData = [{
+        category_name: 'Shirts',
+    },
+    {
+        category_name: 'Shorts',
+    },
+    {
+        category_name: 'Music',
+    },
+    {
+        category_name: 'Hats',
+    },
+    {
+        category_name: 'Shoes',
+    },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+
+module.exports = seedCategories;
