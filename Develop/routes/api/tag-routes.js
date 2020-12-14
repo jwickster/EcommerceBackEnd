a573c63e2e7de95dc9a12d740376057ab6c5e167
Josh Wickster <joshwickster@gmail.com>
2020-12-14 14:10:47 -0700

    fixes post route

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -54,83 +54,79 @@ router.get('/:id', function(req, res) {
             }
         }
 
+        router.post('/', (req, res) => {
+                // create a new tag
+                try {
+                    const newTag = Tag.create(req.body);
+
+                    if (!newTag) {
+                        res.status(404).json({
+                            message: '404 error - cannot find'
+                        });
+
+                    } else if (newTag) {
+
+                        res.status(200).json({
+                            message: '200 - good server'
+                        });
 
 
-        // try {
-        //     const productId = Tag.findByPk(req.params.id); {
-        //         if(!productId){
-        //             res.status(404).json({
-        //                 message: 'Tag not found',
-        //             });
-        //         }
-        //         (200).json(productId); //return 200 code
-        //     } catch (err) {
-        //         console.log(err); //debugging purposes
-        //         res.status(500).json(err); //return a 500 code
-        //     }
-        // });
+                    } else {
+                        res.status(500).json({
+                            message: '500 - bad server'
+                        });
 
+                    }
 
-        router.post('/', function(req, res) {
-            // create a new tag
-            try {
-                const tagData = Tag.create(req.body);
-                res.status(500).json(tagData, {
-                    message: '500 - server side error' + tagData.message,
-                });
-            } catch (err) {
-                res.status(400).json(err, {
-                    message: '400 - Bad Request error + ' + err.message,
-                });
-            } finally {
-                res.status(200).json(tagData, {
-                    message: 'Success!',
-                });
-            }
-        });
-
-        router.put('/:id', function(req, res) {
-            // update a tag's name by its `id` value
-            try {
-                const putData = Tag.update(req.body, {
-                    where: {
-                        id: req.params.id,
-                    },
-                });
-                if (!putData[0]) {
-                    res.status(404).json({
-                        message: 'No tag with this id!'
+                } catch (err) {
+                    res.status(err).json({
+                        message: err.message + 'unknown error' + newTag,
                     });
-                    return;
                 }
-                res.status(200).json(putData);
-            } catch (err) {
-                res.status(500).json(err);
-            }
-        });
+            };
 
-        router.delete('/:id', (req, res) => {
-            // delete on tag by its `id` value
-            try {
-                const deleteTagById = Tag.destroy({
-                    where: {
-                        id: req.params.id,
-                    },
-                });
-
-                if (!deleteTagById) {
-                    res.status(404).json({
-                        message: 'No tag found with that id!'
+            router.put('/:id', function(req, res) {
+                // update a tag's name by its `id` value
+                try {
+                    const putData = Tag.update(req.body, {
+                        where: {
+                            id: req.params.id,
+                        },
                     });
-                } else {
-                    res.status(200).json(deleteTagById);
+                    if (!putData[0]) {
+                        res.status(404).json({
+                            message: 'No tag with this id!'
+                        });
+                        return;
+                    }
+                    res.status(200).json(putData);
+                } catch (err) {
+                    res.status(500).json(err);
+                }
+            });
+
+            router.delete('/:id', (req, res) => {
+                // delete on tag by its `id` value
+                try {
+                    const deleteTagById = Tag.destroy({
+                        where: {
+                            id: req.params.id,
+                        },
+                    });
+
+                    if (!deleteTagById) {
+                        res.status(404).json({
+                            message: 'No tag found with that id!'
+                        });
+                    } else {
+                        res.status(200).json(deleteTagById);
+                    }
+
+                } catch (err) {
+                    res.status(500).json(err);
+
                 }
 
-            } catch (err) {
-                res.status(500).json(err);
+            });
 
-            }
-
-        });
-
-        module.exports = router;
+            module.exports = router;
